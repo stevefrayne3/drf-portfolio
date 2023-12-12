2a3d12850f9fc220c23ff4ddfff8ad3f4c4f6be5
stevefrayne3 <143477554+stevefrayne3@users.noreply.github.com>
2023-12-12 11:20:31 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['yashavasekar.vercel.app' ,'.vercel.app']
+ALLOWED_HOSTS = ['yashavasekar.vercel.app' ,'.vercel.app' ,'172.31.24.64']
 
 
 # ALLOWED_HOSTS = ['*']
